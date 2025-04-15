c7ed9e9291a33e08208e3c88d09fb659b14ff9e7
VISHNU SAI <84028901+vishnusai1569@users.noreply.github.com>
2025-04-15 20:26:05 +0530

    Delete PATTERNS directory

--- PATTERNS/hill pratten.py
+++
@@ -1,9 +0,0 @@
-k=int(input("enter the number:"))
-for i in range(k):
-    for j in range(i,k):
-        print(' ',end='')
-    for j in range(i):
-        print('*',end='')
-    for j in range(i+1):
-        print("*",end='')
-    print()
--- PATTERNS/riverse triangle.py
+++
@@ -1,6 +0,0 @@
-k=int(input("enter the number:"))
-
-for i in range(k):
-    for j in range(i,k):
-        print("*",end='')
-    print()
